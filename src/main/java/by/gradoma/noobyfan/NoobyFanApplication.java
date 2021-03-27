4a6b6c6978dedd12c68a8749a3f3f44a6b086157
Gradoma <gradomski25@gmail.com>
2021-03-27 13:43:53 +0300

    init

---
+++ src/main/java/by/gradoma/noobyfan/NoobyFanApplication.java
@@ -0,0 +1,13 @@
+package by.gradoma.noobyfan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoobyFanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoobyFanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/gradoma/noobyfan/NoobyFanApplicationTests.java
@@ -0,0 +1,13 @@
+package by.gradoma.noobyfan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoobyFanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
